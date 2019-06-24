36aa580c0f2aab166d09369a6f95857c3d1aa893
ronyshchenko <ronyshchenko@gmail.com>
2019-06-24 10:45:16 +0300

    Updates

---
+++ precache-manifest.a4919aac0ee4505b0b4a53fc46f21664.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "475dd56a9d3a6c3cb4fe541f0ebf1a9e",
+    "url": "/react_ekzam/index.html"
+  },
+  {
+    "revision": "d49e56c66c10abf9ed37",
+    "url": "/react_ekzam/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react_ekzam/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d49e56c66c10abf9ed37",
+    "url": "/react_ekzam/static/js/main.223a5da8.chunk.js"
+  },
+  {
+    "revision": "d8274f03da7177fd0def",
+    "url": "/react_ekzam/static/js/runtime~main.8621006b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_ekzam/static/media/logo.5d5d9eef.svg"
+  }
+]);
